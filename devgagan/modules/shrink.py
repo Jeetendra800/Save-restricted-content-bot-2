2b312cb76063e8598ce103819f6e2f53f1a1e725
Shreeraam23 <161189756+Shreeraam23@users.noreply.github.com>
2025-02-02 00:08:37 +0530

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -82,7 +82,7 @@ async def token_handler(client, message):
         ])
          
         await message.reply_photo(
-            image_url, = "https://envs.sh/mDs.jpg"
+            msg.photo.file_id,
             caption=(
                 "Hi 👋 Welcome, Wanna intro...?\n\n"
                 "✳️ I can save posts from channels or groups where forwarding is off. I can download videos/audio from YT, INSTA, ... social platforms\n"
