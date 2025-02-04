7f2acd6737e0ded3c667f23a2e80bfd636fd7564
Shreeraam23 <161189756+Shreeraam23@users.noreply.github.com>
2025-02-04 20:19:29 +0530

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -73,7 +73,7 @@ async def token_handler(client, message):
     msg = await app.get_messages(chat_id, 796)
     user_id = message.chat.id
     if len(message.command) <= 1:
-        image_url = "https://envs.sh/mT_.jpg"
+        image_url = "https://envs.sh/mTG.jpg"
         join_button = InlineKeyboardButton("🎗Join Channel🎗", url="https://t.me/myserver_official")
         premium = InlineKeyboardButton("⚜Get Premium👑", url="https://t.me/Pre_contact_bot")   
         keyboard = InlineKeyboardMarkup([
