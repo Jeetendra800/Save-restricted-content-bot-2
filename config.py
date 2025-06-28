e6647d559c560edaedc7ff715bf89111a4094606
Shreeraam23 <161189756+Shreeraam23@users.noreply.github.com>
2025-06-28 08:51:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", "28036362"))
-API_HASH = getenv("API_HASH", "84e4bd8457dab2f065801bde8874842a")
+API_ID = int(getenv("API_ID", "20373203"))
+API_HASH = getenv("API_HASH", "8962717c7c708e210f66ea658db58d85")
 BOT_TOKEN = getenv("BOT_TOKEN", "7906843564:AAESfB4qXWaNSvHYzYUtSjcE9Fh-c1owDCY")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "7782401729").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7968732891").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://vevob65684:CBT55WcRjWUf0GvN@cluster0.dfd6s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "-1002596328286")
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002413032052"))
