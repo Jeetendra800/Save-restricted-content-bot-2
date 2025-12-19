2cfbe7bc5e697a84662ac36d36d609686f7ce6e7
Shreeraam23 <161189756+Shreeraam23@users.noreply.github.com>
2025-12-19 18:42:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,13 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", "20373203"))
-API_HASH = getenv("API_HASH", "8962717c7c708e210f66ea658db58d85")
-BOT_TOKEN = getenv("BOT_TOKEN", "7906843564:AAH0mKvjm9oWFIBJal76jmdNQ3AvYnUuwAU")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "7968732891").split()))
-MONGO_DB = getenv("MONGO_DB", "mongodb+srv://vevob65684:CBT55WcRjWUf0GvN@cluster0.dfd6s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-LOG_GROUP = getenv("LOG_GROUP", "-1002596328286")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002413032052"))
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://:@cluster0.dfd6s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "10000"))
 WEBSITE_URL = getenv("WEBSITE_URL", "gplinks.com")
