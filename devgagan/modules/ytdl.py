499167516e8340df97f390f673bca1a12a7e1b41
Jeetendra800 <jeetendra2949@gmail.com>
2025-12-20 13:28:20 +0530

    Update ytdl.py

--- devgagan/modules/ytdl.py
+++ devgagan/modules/ytdl.py
@@ -119,8 +119,8 @@ async def process_audio(client, event, url, cookies_env_var=None):
                 except Exception:
                     pass
                 audio_file.tags["TIT2"] = TIT2(encoding=3, text=title)
-                audio_file.tags["TPE1"] = TPE1(encoding=3, text="Team SPY")
-                audio_file.tags["COMM"] = COMM(encoding=3, lang="eng", desc="Comment", text="Processed by RAJPUTSERVER")
+                audio_file.tags["TPE1"] = TPE1(encoding=3, text="GURJAR")
+                audio_file.tags["COMM"] = COMM(encoding=3, lang="eng", desc="Comment", text="Processed by GURJAR")
  
                 thumbnail_url = info_dict.get('thumbnail')
                 if thumbnail_url:
