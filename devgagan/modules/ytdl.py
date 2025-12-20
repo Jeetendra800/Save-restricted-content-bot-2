2951c8567c0b0c8c88cbece32bf97e35df24ff98
Jeetendra800 <jeetendra2949@gmail.com>
2025-12-20 13:18:11 +0530

    Update ytdl.py

--- devgagan/modules/ytdl.py
+++ devgagan/modules/ytdl.py
@@ -148,7 +148,7 @@ async def process_audio(client, event, url, cookies_env_var=None):
                 name=None,
                 progress_bar_function=lambda done, total: progress_callback(done, total, chat_id)
             )
-            await client.send_file(chat_id, uploaded, caption=f"**{title}**\n\n**__Powered by RAJPUTSERVER__**")
+            await client.send_file(chat_id, uploaded, caption=f"**{title}**\n\n**__Powered by GURJAR__**")
             if prog:
                 await prog.delete()
         else:
@@ -302,7 +302,7 @@ def progress_callback(done, total, user_id):
         f"│ **__Speed:__** {speed_mbps:.2f} Mbps\n"
         f"│ **__Time Remaining:__** {remaining_time_min:.2f} min\n"
         f"╰──────────────────╯\n\n"
-        f"**__Powered by RAJPUTSERVER__**"
+        f"**__Powered by GURJAR__**"
     )
  
      
@@ -353,7 +353,7 @@ async def process_video(client, event, url, cookies_env_var, check_duration_and_
             return
          
         await asyncio.to_thread(download_video, url, ydl_opts)
-        title = info_dict.get('title', 'Powered by Team SPY')
+        title = info_dict.get('title', 'Powered by GURJAR')
         k = video_metadata(download_path)      
         W = k['width']
         H = k['height']
